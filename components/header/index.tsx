4ccfffc97295ac69870a700a9c1c147a52dc108a
Stanley-Felix-Bergamo <stanley.felix.bergamo@outlook.com>
2026-03-11 14:05:56 -0300

    commit

---
+++ components/header/index.tsx
@@ -0,0 +1,26 @@
+import Link from "next/link";
+import { Button } from "../ui/button";
+import ActiveLink from "../active-link";
+import Image from "next/image";
+
+export default function Header() {
+  return (
+    <header className="fixed top-0 z-50 w-full border-b border-white/10 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+      <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-80">
+        <div className="flex h-16 items-center justify-between">
+          <Link href={"/"}>
+           <Image alt="Logo site" src={"/logo.svg"} width={116} height={32} />
+          </Link>
+
+          <nav className="flex items-center gap-6">
+            <ActiveLink href={"/"}> Início</ActiveLink>
+            <ActiveLink href={"/blog"}> blog</ActiveLink>
+            <Button variant={"secondary"} asChild>
+              <Link href={"/comecar"}>Começar</Link>
+            </Button>
+          </nav>
+        </div>
+      </div>
+    </header>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,3 @@
-
 export default function HomePage() {
-  return (
-    <div>
-      <h1>Home</h1>
-    </div>
-  );
+  return <div></div>;
 }
