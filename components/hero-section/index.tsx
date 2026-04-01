aaf64b6af74033888934b395dc8ea8a3dd44ac4a
Stanley-Felix-Bergamo <stanley.felix.bergamo@outlook.com>
2026-04-01 15:24:45 -0300

    commit

--- components/hero-section/index.tsx
+++ components/hero-section/index.tsx
@@ -8,7 +8,7 @@ export default function HeroSection() {
     <section className="container relative flex items-center justify-center mt-16">
       <div className="grid grid-cols-1 md:grid-cols-2 gap-8 items-center">
         <div className="flex flex-col items-center justify-center gap-4 md:items-start mt-16 lg:items-start">
-          <h1 className="text-gray-100 text-heading-hg  text-center md:text-left lg:text-left">
+          <h1 className="text-gray-100 text-heading-hg  text-center md:text-left lg:text-left font-sans">
             Venda seus produtos como afiliado em um único lugar
           </h1>
 
