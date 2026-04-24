2d48788831b1dee5a7c3b4ff78ab887454d8e53f
Stanley-Felix-Bergamo <stanley.felix.bergamo@outlook.com>
2026-04-24 14:09:58 -0300

    commit

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,12 @@
 import type { NextConfig } from "next";
+// next.config.js
+import { withContentlayer } from 'next-contentlayer2';
 
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
 };
 
-export default nextConfig;
+export default withContentlayer(nextConfig);
+
+
